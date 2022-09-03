af89f20d933bd0bcfa8e7887d432539a6534deee
ritik varshney <ritik.varshney_cs19@gla.ac.in>
2022-09-03 11:43:58 +0530

    FirstJavaProgram

---
+++ src/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        add(1,2);
+    }
+    public static void add(int n, int m) {
+        System.out.println(m+n);
+    }
+}
